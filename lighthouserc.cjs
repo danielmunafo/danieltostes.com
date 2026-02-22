3edee67a9d89182f6e1c0795b590cbe35d899768
Daniel Munafó Tostes <dann.tostes@gmail.com>
2026-02-22 22:25:40 +0100

    ci: add lighthouse

---
+++ lighthouserc.cjs
@@ -0,0 +1,24 @@
+/** @see https://github.com/GoogleChrome/lighthouse-ci/blob/main/docs/configuration.md */
+module.exports = {
+  ci: {
+    collect: {
+      staticDistDir: "./out",
+      url: ["http://localhost/en.html"],
+      numberOfRuns: 1,
+      settings: {
+        chromeFlags: "--no-sandbox --headless --disable-gpu",
+      },
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["error", { minScore: 0.95 }],
+        "categories:accessibility": ["error", { minScore: 0.95 }],
+        "categories:best-practices": ["error", { minScore: 0.95 }],
+        "categories:seo": ["error", { minScore: 0.95 }],
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
